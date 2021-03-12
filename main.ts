f23c1c9cf574a5b5ec94b55189404f6bf67a9922
Artur Herczeg <artur.herczeg@bitrise.io>
2021-03-12 16:00:47 +0100

    Basic implementation of toUpper, ToTitle

--- main.ts
+++ main.ts
@@ -17,19 +17,58 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		this.addCommand({
-			id: 'lowercase',
+			id: 'to-lowercase',
 			name: 'To Lowercase',
-			checkCallback: (checking: boolean) => {
-				let editor = this.getEditor();
-				if(!checking) {
-					editor.replaceSelection(this.getSelectedText(editor).toLowerCase());
-				}
-				console.log('TO LOWERCASE');
-				return true;
-			}
+			checkCallback: this.toLowecase.bind(this)
+		});
+
+		this.addCommand({
+			id: 'to-uppercase',
+			name: 'To Uppercase',
+			checkCallback: this.toUppercase.bind(this)
+		});
+
+		this.addCommand({
+			id: 'to-titlecase',
+			name: 'To Titlecase',
+			checkCallback: this.toTitleCase.bind(this)
 		});
 
 		//this.addSettingTab(new SampleSettingTab(this.app, this));
+	}
+
+	private toLowecase(checking: boolean) {
+		let editor = this.getEditor();
+		if (!checking) {
+			let text = this.getSelectedText(editor);
+			editor.replaceSelection(text.toLowerCase());
+		}
+		console.log('TO LOWERCASE');
+		return true;
+	}
+
+	private toUppercase(checking: boolean) {
+		let editor = this.getEditor();
+		if (!checking) {
+			let text = this.getSelectedText(editor);
+			editor.replaceSelection(text.toUpperCase());
+		}
+		console.log('TO UPPERCASE');
+		return true;
+	}
+
+	private toTitleCase(checking: boolean){
+		let editor = this.getEditor();
+		let text = this.getSelectedText(editor);
+
+		if (!checking) {
+		let lowercase = text.toLowerCase();
+		let titleCase = lowercase.substring(0, 1).toUpperCase() + lowercase.substring(1);
+
+		editor.replaceSelection(titleCase);
+		}
+
+		return true;
 	}
 
 	onunload() {
