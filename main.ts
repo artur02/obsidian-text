ca8a59af5fecca9415cc5b29f27889f2fced311b
Artur Herczeg <artur.herczeg@bitrise.io>
2021-03-12 16:05:35 +0100

    Revoved unused sample code

--- main.ts
+++ main.ts
@@ -1,20 +1,9 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+import { Plugin } from 'obsidian';
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
 
 	async onload() {
 		console.log('loading plugin');
-
-		await this.loadSettings();
 
 		this.addCommand({
 			id: 'to-lowercase',
@@ -69,22 +58,7 @@ export default class MyPlugin extends Plugin {
 		}
 
 		return true;
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-
-	
-	
+	}	
 	
 	private getEditor(): CodeMirror.Editor {
 		let activeLeaf: any = this.app.workspace.activeLeaf;
@@ -93,34 +67,5 @@ export default class MyPlugin extends Plugin {
 
 	private getSelectedText(editor : CodeMirror.Editor) : string {
 		return editor.getSelection();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
