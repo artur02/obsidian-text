b296c5ff2f3b441cd851a46df15a84e6bf4f7d7f
Artur Herczeg <artur.herczeg@bitrise.io>
2021-03-17 19:24:01 +0100

    tests for lowerCase, titleCase, capitalize

--- test/test.spec.ts
+++ test/test.spec.ts
@@ -14,5 +14,47 @@ describe("Case", () => {
                 expect(c.upperCase(text)).to.equal(expected);
             });
         });
-    })
+    });
+
+    describe("lowerCase", () => {
+        const tests = [
+            {text: "thiS iS a Simple teXt", expected: "this is a simple text"},
+            {text: "tHis Is!@#$%ˆ&*()", expected: "this is!@#$%ˆ&*()"}
+        ];
+        
+        tests.forEach(({text, expected}) => {
+            it(`"${text}" should be converted to "${expected}"`, () => {
+                const c = new Case();
+                expect(c.lowerCase(text)).to.equal(expected);
+            });
+        });
+    });
+
+    describe("titleCase", () => {
+        const tests = [
+            {text: "this is, a    siMple text", expected: "This Is, A    SiMple Text"},
+            {text: "this is word-producing text -idea", expected: "This Is Word-producing Text -idea"}
+        ];
+        
+        tests.forEach(({text, expected}) => {
+            it(`"${text}" should be converted to "${expected}"`, () => {
+                const c = new Case();
+                expect(c.titleCase(text)).to.equal(expected);
+            });
+        });
+    });
+
+    describe("capitalize", () => {
+        const tests = [
+            {text: "thiS iS, a Simple teXt", expected: "This is, a simple text"},
+            {text: "this is word-producing text -idea", expected: "This is word-producing text -idea"}
+        ];
+        
+        tests.forEach(({text, expected}) => {
+            it(`"${text}" should be converted to "${expected}"`, () => {
+                const c = new Case();
+                expect(c.capitalize(text)).to.equal(expected);
+            });
+        });
+    });
 });
